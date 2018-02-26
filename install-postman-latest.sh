7433a4868b8b7ef7c07264f098f404895d15ca1b
Kamlesh Patel <kamlesh.patel.djs@gmail.com>
2018-02-26 13:00:39 +0530

    Update install-postman-latest.sh

--- install-postman-latest.sh
+++ install-postman-latest.sh
@@ -1,5 +1,5 @@
 wget https://dl.pstmn.io/download/latest/linux64 -O /tmp/postman.tar.gz
 sudo tar -xzf /tmp/postman.tar.gz -C /opt
-rm postman.tar.gz
+rm /tmp/postman.tar.gz
 sudo rm /usr/bin/postman
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
