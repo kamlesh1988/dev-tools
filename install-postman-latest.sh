6841a8b412ce7844ac54d909b0c394a70237c0e1
Kamlesh Patel <kamlesh.patel@gupshup.io>
2018-05-30 16:54:47 +0530

    Added script for generating desktop icon for postman

--- install-postman-latest.sh
+++ install-postman-latest.sh
@@ -3,3 +3,14 @@ sudo tar -xzf /tmp/postman.tar.gz -C /opt
 rm /tmp/postman.tar.gz
 sudo rm /usr/bin/postman
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+echo '[Desktop Entry]
+Name=Postman
+Type=Application
+Exec=/opt/Postman/Postman
+Terminal=false
+Icon=/opt/Postman/resources/app/assets/icon.png
+Comment=Postman
+NoDisplay=false
+Categories=Development;IDE;
+Name[en]=Postman' > /home/$USER/.local/share/applications/postman.desktop
