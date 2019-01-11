30e095dcafca12ef0b378a67a3ec1abded28a4c3
kamlesh patel <kamlesh.patel@gupshup.io>
2019-01-11 12:27:42 +0530

    Added script which helps to add eclipse desktop icon

---
+++ create-eclipse-shortcut.sh
@@ -0,0 +1,5 @@
+cd ~/.local/share/applications
+wget https://gist.github.com/raw/2922285/eclipse.desktop
+chmod u+x eclipse.desktop
+vim eclipse.desktop
+# edit eclipse.desktop to replace "<ECLIPSE_DIR>" with your eclipse directory
