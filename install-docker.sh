3a28441f83aa4a10258129de13dba23a082d4e8f
kamlesh patel <kamlesh.patel@gupshup.io>
2019-02-11 15:11:55 +0530

    Updated installation scripts for postman and docker

--- install-docker.sh
+++ install-docker.sh
@@ -25,9 +25,16 @@ sudo su
 sudo mkdir -p /etc/systemd/system/docker.service.d/
 
 # /etc/systemd/system/docker.service.d/override.conf
-sudo echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2377" > /etc/systemd/system/docker.service.d/override.conf
+echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375" > /etc/systemd/system/docker.service.d/override.conf
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
-sudo systemctl restart docker.service
+systemctl restart docker.service
 
+# Navigate to /lib/system/system in your terminal and open docker.service file
+# vi /lib/systemd/system/docker.service
+# Find the line which starts with ExecStart and adds  -H=tcp://0.0.0.0:2375 to make it look like
+# ExecStart=/usr/bin/docker daemon -H=fd:// -H=tcp://0.0.0.0:2375
+
+# For latest version of docker 18.xx
+#vim /etc/default/docker
